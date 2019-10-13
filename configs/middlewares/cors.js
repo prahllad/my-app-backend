19f9dc398be2ae7f2aacb26bd28a789e03656fd9
prahllad roy <royprahllad3@gmail.com>
2019-10-13 13:20:43 +0530

    changes

--- configs/middlewares/cors.js
+++ configs/middlewares/cors.js
@@ -1,7 +1,7 @@
 'use strict';
 const cors = require('cors');
 module.exports = cors({
-    origin: ['http://localhost:4200','https://first-angular1-app1.herokuapp.com'],
+    origin: ['http://localhost:4200','https://first-angular1-app1.herokuapp.com','*'],
     methods: ['OPTIONS', 'GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     allowedHeaders: ['Origin', 'X-Requested-With', 'X-HTTP-Method-Override', 'Content-Type', 'Accept', 'Content-Encoding', 'Authorization', 'api-key'],
     credentials: true
