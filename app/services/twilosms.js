4cbafe395497737b4114f939ff5f5550378b3577
Prahllad Roy <royprahllad3@gmail.com>
2020-05-05 02:55:14 +0530

    fixes and changes

--- app/services/twilosms.js
+++ app/services/twilosms.js
@@ -1,20 +1,18 @@
-const accountSid = 'ACd77811db734c88b0036d26db8197c046';
-const authToken = '8adc8d78a20c25e14e5a66a51e044bf5';
-const USID = 'VA40a1e3876c415c66e2fd9599a1ee1777';
-const client = require('twilio')(accountSid, authToken);
+
+const client = require('twilio')(process.env.accountSid,process.env.authToken);
 const self = module.exports = {
 
   sendCode:async(data)=>{
    
     
-    await client.verify.services(USID)
+    await client.verify.services(process.env.USID)
                  .verifications
                  .create({to: '+91'+data.phone, channel: 'sms'})
                  .then(verification => {return verification});
   },
   verifyCode:async(data)=>{
     try{
-       let verification_check = await client.verify.services(USID)
+       let verification_check = await client.verify.services(process.env.USID)
           .verificationChecks
           .create({to: '+91'+data.phone, code: data.code});
           return verification_check;
